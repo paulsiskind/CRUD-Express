89146a3888dfd8910fea293e3c9199d2299816a6
paulsiskind <paulsiskind@gmail.com>
2015-09-13 15:24:02 -0600

    Frame Worked

--- routes/albums.js
+++ routes/albums.js
@@ -10,21 +10,12 @@ router.get('/albums', function(req, res, next) {
   albumCollection.find({}, function (err, records) {
     res.render('albums/index', {allAlbums: records});
   });
+});
 
 router.get('/albums/new', function(req, res, next) {
   res.render('albums/new');
 });
 
-router.get('/albums/:id', function(req, res, next) {
-  albumCollection.findOne({_id: req.params.id}, function (err, record) {
-    res.render('albums/show', {theAlbum: record});
-  });
-});
-router.get('/albums/:id/update', function(req, res, next){
-  albumCollection.findOne({_id: req.params.id}, function (err, record) {
-  res.render('albums/edit',{theAlbum: record});
- });
-});
 
 router.post('/albums', function(req, res, next) {
   albumCollection.insert({ name: req.body.album_name,
@@ -35,6 +26,36 @@ router.post('/albums', function(req, res, next) {
   res.redirect('/albums');
 });
 
+
+router.get('/albums/:id', function(req, res, next) {
+  albumCollection.findOne({_id: req.params.id}, function (err, record) {
+    res.render('albums/show', {theAlbum: record});
+  });
 });
+
+router.post('/albums/:id/update', function(req, res, next) {
+  console.log("are you there")
+  albumCollection.updateById( req.params.id,{ name: req.body.album_name,
+  artist: req.body.album_artist,
+  genre: req.body.album_genre,
+  rating: req.body.album_rating,
+  explicit: req.body.album_explicit }, function(err, record){
+  res.redirect('/albums');
+});
+});
+
+
+router.post('/albums/:id/delete', function(req, res, next) {
+  albumCollection.remove({_id: req.params.id }, function (err, record) {
+    res.redirect('/albums/');
+  });
+});
+
+router.get('/albums/:id/edit', function(req, res, next){
+  albumCollection.findOne({_id: req.params.id}, function (err, record) {
+  res.render('albums/edit',{theAlbum: record});
+ });
+});
+
 
 module.exports = router;
